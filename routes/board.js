4ce98029c3e1ce3c1b214d3e88a75d04f362c2f3
kyu9341 <kyu9341@naver.com>
2020-06-16 01:53:35 +0900

    네비게이션바 및 로고 수정

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { Post, User, Like } = require('../models');
 
+router.get('/list/:page', async (req, res, next) => {
+   const page = req.params.page;
 
+});
 
 
 module.exports = router;
