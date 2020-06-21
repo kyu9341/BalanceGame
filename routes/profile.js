2a8cf84aaceb6c6e088732b053a5665069624690
cherrytomato1 <terec@naver.com>
2020-06-21 20:18:57 +0900

    프로필 인증추가

--- routes/profile.js
+++ routes/profile.js
@@ -6,18 +6,6 @@ const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { User } = require('../models');
 
-router.get('/', isLoggedIn, async(req, res, next) => {
-    try {
-        await res.render('/profile', {
-            user:req.user,
-            title: 'Profile page',
-        });
-        console.log("profile response");
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
 
 router.get('/edit', isLoggedIn, async(req, res, next) =>{
     try{
@@ -34,6 +22,40 @@ router.get('/edit', isLoggedIn, async(req, res, next) =>{
     }
 });
 
+router.get('/auth', isLoggedIn, async(req, res, next) =>{
+    try{
+        await res.render('profile/auth',{
+            user:req.user,
+            title: 'profile edit auth',
+
+        });
+
+        console.log("edit-auth");
+    } catch(error){
+        console.error(error);
+        next(error);
+    }
+});
+
+router.post('/auth/submit',isLoggedIn, async(req, res, next) =>{
+
+    const password = req.body.password;
+    //console.log(password+User.password);
+    try {
+        const result = await bcrypt.compare(password, req.user.password);
+
+        if(!result) {
+            req.flash('profileAuthError', '비밀번호가 맞지 않습니다.');
+            return res.redirect('/auth');
+        }
+        return res.redirect('/profile/edit');
+    }
+    catch(error){
+        console.error(error);
+        next(error);
+    }
+
+})
 
 router.post('/edit/submit', isLoggedIn, async (req, res, next) => {
     console.log("tomaot");
