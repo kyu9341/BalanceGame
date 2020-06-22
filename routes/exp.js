a9a4ad0c1e6c0c7c33e13a5e58c81426bdb4963d
cherrytomato1 <terec@naver.com>
2020-06-23 01:14:40 +0900

    레벨별 이미지 추가 및 레벨- 이미지 url 연결

--- routes/exp.js
+++ routes/exp.js
@@ -61,71 +61,34 @@ exports.lvPrint = async (id)=> {
     try{
         const user = await User.findOne({
             where : { id },
-            attributes : ['exp'],
         });
         const exp = user.exp;
 
         console.log(id+exp+"exptest");
         //const exp = await
-        let lv = 1;
         let next = 0;
-        let imgUrl = '/images/egg.png';
 
-        if(exp<100){
-            lv=1;
+        if(exp<100)
             next = 100;
-        }
-        else if(exp<250){
-            lv=2;
+        else if(exp<250)
             next = 250;
-        }
-        else if(exp<450){
-            lv=3;
+        else if(exp<450)
             next = 450;
-        }
-        else if(exp<750){
-            lv=4;
+        else if(exp<750)
             next = 750;
-        }
-        else if(exp<1200){
-            lv=5;
+        else if(exp<1200)
             next = 1200;
-        }
-        else{
-            lv=6;
+        else
             next = 5000;
-        }
 
-        switch(lv) {
-            case 1 :
-                imgUrl = '/images/egg.png';
-                break;
-            case 2 :
-                imgUrl = '/images/breakegg.png';
-                break;
-            case 3 :
-                imgUrl = '/images/toleft.png';
-                break;
-            case 4 :
-                imgUrl = '/images/toright.png';
-                break;
-            case 5 :
-                imgUrl = '/images/chicken.png';
-                break;
-            case 6 :
-                imgUrl = '/images/chicken.png';
-                break;
-        }
 
         const per = Math.round(exp/next*100);
 
-        console.log(imgUrl+'tomato2');
         return lvInfo = {
-            level:lv,
             reqExp:next,
             exp:exp,
             per: per,
-            imgUrl : imgUrl,
+            level : user.level,
         };
 
     } catch(error) {
